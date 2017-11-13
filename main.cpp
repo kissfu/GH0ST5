7491a032aece6f4403b113c5c61c478474ab7969
zoneWalker9k1@gmail.com <zoneWalker9k1@gmail.com>
2017-11-13 15:28:35 -0500

    by Zeex@github

--- main.cpp
+++ main.cpp
@@ -6,7 +6,15 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<signal.h>
 using namespace std;
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT)
+    printf("received SIGINT\n");
+}
+
 
 void error( char *msg)
 {
@@ -19,6 +27,7 @@ int main()
 	sockfd = socket(AF_INET,SOCK_DGRAM,0);
 	struct sockaddr_in serv,client;
  
+	/* Network setup */
 	serv.sin_family = AF_INET;
 	serv.sin_port = htons(53000);
 	serv.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -27,9 +36,17 @@ int main()
 	socklen_t l = sizeof(client);
 	socklen_t m = sizeof(serv);
 	//socklen_t m = client;
-	cout<<"\ngoing to send\n";
-	cout<<"\npls enter the mssg to be sent\n";
-	fgets(buffer,256,stdin);
-	sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr *)&serv,m);
+	//cout<<"\ngoing to send\n";
+	//cout<<"\npls enter the mssg to be sent\n";
+	//fgets(buffer,256,stdin);
+
+	/* Signal setup */
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+		printf("\ncan't catch SIGINT\n");
+
+	while(true)
+	{
+		sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr *)&serv,m);
+	}
 	recvfrom(sockfd,buffer,256,0,(struct sockaddr *)&client,&l);
 }
